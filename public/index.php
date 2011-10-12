dd01db7f8fd1c1a638929515b07fafade535d06c
Luke Barton <lukembarton@gmail.com>
2011-10-12 23:43:56 +0100

    Corrected all PHP file coding standards issues

--- public/index.php
+++ public/index.php
@@ -2,17 +2,28 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define(
+        'APPLICATION_PATH',
+        realpath(dirname(__FILE__) . '/../application')
+    );
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    || define(
+        'APPLICATION_ENV',
+        (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production')
+    );
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array(
+            realpath(APPLICATION_PATH . '/../library'),
+            get_include_path(),
+        )
+    )
+);
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -5,13 +5,20 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->bootstrap = new Zend_Application(
+            APPLICATION_ENV,
+            APPLICATION_PATH . '/configs/application.ini'
+        );
         parent::setUp();
     }
 
     public function testIndexAction()
     {
-        $params = array('action' => 'index', 'controller' => 'Index', 'module' => 'default');
+        $params = array(
+            'action' => 'index',
+            'controller' => 'Index',
+            'module' => 'default'
+        );
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
         $this->dispatch($url);
@@ -20,7 +27,10 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains("div#welcome h3", "This is your project's main page");
+        $this->assertQueryContentContains(
+            "div#welcome h3",
+            "This is your project's main page"
+        );
     }
 
 
