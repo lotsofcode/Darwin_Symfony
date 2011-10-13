e9efc3ae1ab1fe221dcde6fd9735caa49c508bc5
Luke Barton <lukembarton@gmail.com>
2011-10-13 01:13:45 +0100

    Removed redundant pear configuration call in the build tools installation script

--- bin/install-build-tools.sh
+++ bin/install-build-tools.sh
@@ -1,7 +1,5 @@
 hash pear 2>&- || { echo >&2 "I require pear but it's not installed.  Aborting."; exit 1; }
 
-pear config-set auto_discover 1
-
 pear install pear.phing.info/phing
 pear install pear.phpunit.de/PHPUnit
 pear install PHP_CodeSniffer
